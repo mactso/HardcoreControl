dab1ab85c44d72caa277700b37270ac805466d7e
MacTso <stephenmcleod2004@yahoo.com>
2023-01-11 17:14:15 -0600

    Create Main.java

---
+++ src/main/java/com/mactso/hardcorecontrol/Main.java
@@ -0,0 +1,15 @@
+package com.mactso.hardcorecontrol;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("hardcorecontrol")
+public class Main {
+
+	    public static final String MODID = "hardcorecontrol"; 
+	    
+	    public Main()
+	    {
+	    	System.out.println(MODID + ": Registering Mod.");
+	    }
+
+}
